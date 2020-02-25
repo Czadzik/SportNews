70c079ac48df1f1d7d56c271e2ce2e6ef3599a10
Karol Weglarz <dns997@gmail.com>
2020-02-25 23:17:42 +0100

    Add initial files

---
+++ SportNews/ViewModels/Utils/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraphGenerator.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        protected void OnPropertyChanged([CallerMemberName] string memberName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
