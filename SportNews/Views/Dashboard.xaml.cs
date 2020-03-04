7268260b30ba76e759874e56571927b83b9e03a8
Kamil Mentel <mentixx.97@gmail.com>
2020-03-04 01:08:51 +0100

    Dodanie Packages i stworzenie PopUpMenu

---
+++ SportNews/Views/Dashboard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SportNews
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPopUpCloseAplication_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+    }
+}
