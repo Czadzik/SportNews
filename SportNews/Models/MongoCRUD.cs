fae1117ad59d2a6acb726b6d959724f31afb518c
Karol Weglarz <dns997@gmail.com>
2020-03-06 14:11:45 +0100

    Dodalem  CRUDMongo

---
+++ SportNews/Models/MongoCRUD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace SportNews.Models
+{
+    class MongoCRUD
+    {
+        private IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+        public List<T> LoadRecords<T>(string table)
+        {
+            var colection = db.GetCollection<T>(table);
+            return colection.Find(new BsonDocument()).ToList();
+        }
+        public void DeleteRecord<T>(string table, int id)
+        {
+            var colection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            colection.DeleteOne(filter);
+        }
+        public void UpsertRescord<T>(string table, int id, T record)
+        {
+            var colection = db.GetCollection<T>(table);
+            var result = colection.ReplaceOne(new BsonDocument("_id", id), record, new UpdateOptions { IsUpsert = true });
+        }
+    }
+}
