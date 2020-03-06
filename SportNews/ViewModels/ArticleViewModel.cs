5392a4801ef2627f4c5467986e630a9670f7c648
mcw256 <rivael256@gmail.com>
2020-03-06 13:24:01 +0100

    dodałem okno z artykułem

---
+++ SportNews/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SimpleGraphGenerator.ViewModels.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportNews.ViewModels
+{
+    class ArticleViewModel : ViewModelBase
+    {
+        public ArticleViewModel(string articleId)
+        {
+
+
+        }
+    }
+}
